367f1d4da4f9e3a1b3fbf60e971744bc05e4c925
SAIF-ANWAR <saifanwar5123@gmail.com>
2022-05-01 01:58:19 +0600

    find-collection

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -12,12 +12,30 @@ app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kbd0s.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-});
 
+
+async function run() {
+    await client.connect()
+    const laptopCollection = client.db("inventory").collection("laptops");
+    try {
+        app.get('/laptops', async (req, res) => {
+            const query = {}
+            const cursor = laptopCollection.find(query)
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+        app.get('/laptops/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const cursor = await laptopCollection.findOne(query)
+            res.send(cursor)
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir)
 
 app.get('/', (req, res) => {
     res.send('server site is running')
